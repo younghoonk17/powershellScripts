e0fc2acf799a92f91b2a24ebf36f731d9e02ba2a
Unknown <c.yhkim17@gmail.com>
2018-09-23 21:03:51 +1200

    bus timetable script

---
+++ busTimeTable.ps1
@@ -0,0 +1,37 @@
+﻿
+function busAPI {
+    
+    $url = "https://api.at.govt.nz/v2/public-restricted/departures/1090?subscription-key=323741614c1c4b9083299adefe100aa6&hours=1&rowCount=1&isMobile=false&mobileRowCount=15&_=1537610771739"
+    [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+
+    $result = Invoke-WebRequest $url -Method Get | ConvertFrom-Json
+
+    $busNumber = $args[0]
+
+
+    if ( ([string]::IsNullOrEmpty($busNumber))){
+        $result.response.movements | Select scheduledArrivalTime, expectedArrivalTime, route_short_name, destinationDisplay
+    }
+    else{
+        $result.response.movements | Select scheduledArrivalTime, expectedArrivalTime, route_short_name, destinationDisplay | Where {$_.route_short_name -eq $busNumber};
+    }
+
+
+    if ( $args[1] -gt 0){
+        Start-Sleep 30
+    }
+
+    
+}
+
+function repeat {
+    $counter = 0
+
+    For(;;){
+        busAPI $args[0] $counter
+        $counter++
+    }
+}
+
+repeat 875
+
